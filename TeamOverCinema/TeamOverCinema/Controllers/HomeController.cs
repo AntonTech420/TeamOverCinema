dfa2db347f9f114871a620d2b94b0c724e640755
NiwaNeko299 <73807808+JoshuaAPetersen@users.noreply.github.com>
2022-05-31 13:40:54 +1200

    Home Page Movies Added

--- TeamOverCinema/TeamOverCinema/Controllers/HomeController.cs
+++ TeamOverCinema/TeamOverCinema/Controllers/HomeController.cs
@@ -1,26 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using TeamOverCinema.Models;
+using Microsoft.EntityFrameworkCore;
+using TeamOverCinema.Data;
 
 namespace TeamOverCinema.Controllers
 {
+        
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ApplicationDbContext context)
         {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
+            _context = context;
         }
 
         public IActionResult Admin()
@@ -37,10 +30,6 @@ namespace TeamOverCinema.Controllers
             return View();
         }
         public IActionResult FoodandDrink()
-        {
-            return View();
-        }
-        public IActionResult Promotions()
         {
             return View();
         }
@@ -64,6 +53,56 @@ namespace TeamOverCinema.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        // GET: Events
+        public async Task<IActionResult> Promotions()
+        {
+            return _context.Events != null ?
+                        View(await _context.Events.ToListAsync()) :
+                        Problem("Entity set 'ApplicationDbContext.Events'  is null.");
+        }
+
+        // GET: Events/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Events == null)
+            {
+                return NotFound();
+            }
+
+            var events = await _context.Events
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (events == null)
+            {
+                return NotFound();
+            }
+
+            return View(events);
+        }
+        // GET: Movies
+        public async Task<IActionResult> Index()
+        {
+            return _context.Movies != null ?
+                        View(await _context.Movies.ToListAsync()) :
+                        Problem("Entity set 'ApplicationDbContext.Movies'  is null.");
+        }
+
+        // GET: Movies/Details/5
+        public async Task<IActionResult> MDetails(int? id)
+        {
+            if (id == null || _context.Movies == null)
+            {
+                return NotFound();
+            }
+
+            var movies = await _context.Movies
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (movies == null)
+            {
+                return NotFound();
+            }
+
+            return View(movies);
         }
     }
 }
