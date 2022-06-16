0114d7540cd9c603e790b44292d6560cf8b8c937
AMW <73808213+AntonTech420@users.noreply.github.com>
2022-06-17 10:26:17 +1200

    Times and Seats changes

--- TeamOverCinema/TeamOverCinema/wwwroot/js/site.js
+++ TeamOverCinema/TeamOverCinema/wwwroot/js/site.js
@@ -5,7 +5,7 @@
 
 
 const container = document.querySelector('.container');
-const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const seats = document.querySelectorAll('.roww .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
@@ -22,7 +22,7 @@ function setMovieData(movieIndex, moviePrice) {
 
 // Update total and count
 function updateSelectedCount() {
-    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+    const selectedSeats = document.querySelectorAll('.roww .seat.selected');
 
     const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
 
